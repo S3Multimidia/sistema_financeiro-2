fb2bbd696b7206f4a88adeb96ba24cff4d1fa57f
Danilo Xavier <s3multimidia@gmail.com>
2026-01-18 14:14:33 -0300

    chore: update supabase default config

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Default keys (fallbacks)
-const DEFAULT_URL = 'https://ujhdduyljjdkdblhtqic.supabase.co';
-const DEFAULT_KEY = 'sb_publishable_H3WXUMpY4aHZiAXaxOf1dA_XQZl4-Qu';
+// Default keys (fallbacks)
+const DEFAULT_URL = 'https://bysvhhtgutdzzjoymjrf.supabase.co';
+const DEFAULT_KEY = 'sb_publishable_a5SjPIzZl_u_7mWI-wvh_g_rbYv85EH';
 
 // Get from env (Vite/Vercel), localStorage or use default
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || localStorage.getItem('supabase_url') || DEFAULT_URL;
