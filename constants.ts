5b5c1d96f9e5a07af8085c5157af4e562d85a6d0
Danilo Xavier <s3multimidia@gmail.com>
2026-01-18 17:59:35 -0300

    chore: bump version to 3.0

--- constants.ts
+++ constants.ts
@@ -2,7 +2,7 @@
 import { Transaction, INITIAL_CATEGORIES_MAP } from './types';
 
 export const INITIAL_PREVIOUS_BALANCE = 0.00;
-export const APP_VERSION = '2.7';
+export const APP_VERSION = '3.0';
 
 /**
  * CONFIGURAÇÃO DA API PERFEX
